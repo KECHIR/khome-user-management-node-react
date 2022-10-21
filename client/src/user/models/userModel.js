fbe8d04868f1a8ab6d74d888937f9856130178e7
Riad KECHIR <kechir@SNEW.localdomain>
2022-10-21 17:44:43 +0200

    Add userModel

---
+++ client/src/user/models/userModel.js
@@ -0,0 +1,11 @@
+import * as Yup from 'yup';
+
+const initialValues = { lastName: "", firstName: "", email: "" };
+
+const validationSchema = Yup.object().shape({
+    lastName: Yup.string().required("Le nom de l'utilisateur est obligatoire"),
+    firstName: Yup.string().required("Le prénom de l'utilisateur est obligatoire"),
+    email: Yup.string().required("L'adresse e-mail est obligatoire ").email('Veuillez entrer une adresse e-mail valide'),
+});
+
+export { initialValues, validationSchema };
